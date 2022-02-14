59ca9a103a5b50384c5c0a15df1912dda84e708a
Yabo <yabdala@indevs.site>
2022-02-14 12:19:21 -0700

    add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ const data = [
   {
     title: 'Odoo Implementation',
     subtitle: 'Trabis - Coinsamatik - Indevs',
+    link: 'https://indevs.site',
     image: '/images/projects/nearestdollar.jpg',
     date: '2019-11-20',
     desc:
@@ -13,15 +14,13 @@ const data = [
       + 'Custom modules for most departments',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
+    title: 'Inventory for local food business',
+    subtitle: 'Inventory Management',
     image: '/images/projects/harvest.jpg',
     date: '2015-09-20',
     desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+      + 'Vanilla Bootstrap-PHP project'
+      + 'Basic functionallity',
   },
 
 
