6d05694ca1d5828e8f75205d55837f27cc2d60fb
Yabo <yabdala@indevs.site>
2022-02-14 12:01:59 -0700

    typos in about

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -23,7 +23,7 @@ const SideBar = () => (
       I am the founder of <a href="https://indevs.site">Indevs</a> a software consulting company and I have professional experience as sysadmin for multiple companies.
         Before Indevs I was at <a href="https://www.blharbert.com/">BL Harbert</a>
         , <a href="https://coinsamatik.com/">CoinsaMatik</a>
-        , and <a href="https://www.trabis.com.mx/">Trabis</a>.
+        , and <a href="https://www.trabis.com.mx/">Trabis</a>
       </p>
       <ul className="actions">
         <li>
