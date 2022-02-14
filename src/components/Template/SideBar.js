9e21e338e41b7a1733614c6b2cf425e15fafe7cf
Yabo <yabdala@indevs.site>
2022-02-14 09:36:52 -0700

    update sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -20,7 +20,7 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>I&#39;m a software engineer from <a href="https://www.unadmexico.mx/">UNADM</a> with proficiency in backend technologies such as Python, PHP, Nginx, Odoo ERP, Flask, and Laravel.
-      I am the founder of <a href="https://indevs.site">Indevs</a> a software consulting company and I also have professional experience as a sysadminand for several companies.
+      I am the founder of <a href="https://indevs.site">Indevs</a> a software consulting company and I have professional experience as a sysadmin for multiple companies.
         Before Indevs I was at <a href="https://www.blharbert.com/">BL Harbert</a>
         , <a href="https://coinsamatik.com/">CoinsaMatik</a>
         , and <a href="https://www.trabis.com.mx/">Trabis</a>.
